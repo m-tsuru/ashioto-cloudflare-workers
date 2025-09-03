c8067f59404d2993c03cfb748503a19fdf8505c6
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2025-09-04 00:09:08 +0900

    feat: update dependencies and remove unused favicon and assetsignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
 import { defineConfig } from 'vite'
 import ssrPlugin from 'vite-ssr-components/plugin'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [cloudflare(), ssrPlugin(), svelte()]
+  plugins: [cloudflare(), ssrPlugin()]
 })
